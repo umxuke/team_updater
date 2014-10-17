5cb9b0a1e2e52fd7b4095d796f2115d29f16045d
Ke Xu <xuke@umich.edu>
2014-10-16 23:37:17 -0700

    debug

--- comment/commentajax.php
+++ comment/commentajax.php
@@ -22,20 +22,19 @@ mysql_query("insert into posts (user_name,post_dis) values ('$username','$commen
 
 }
 
-header('Location:');
 ?>
 
 
 <?php 
-// $time = date('Y-m-d H:i:s');
-// 
-// echo "<div class='post'>";
-// echo "<img src='../views/img/avatar_standard.jpg' width='50' height='50'>";
-// echo "<span class='post_info'><span class='post_name'>";
-// echo $username, "</span><span class='post_time'> ($time)</span></span><span class='post_comment'> ";
-// echo $comment;
-// echo "</span><br>";
-// echo "</div>";
+$time = date('Y-m-d H:i:s');
+
+echo "<div class='post'>";
+echo "<img src='../views/img/avatar_standard.jpg' width='50' height='50'>";
+echo "<span class='post_info'><span class='post_name'>";
+echo $username, "</span><span class='post_time'> ($time)</span></span><span class='post_comment'> ";
+echo $comment;
+echo "</span><br>";
+echo "</div>";
 ?>
 
 
